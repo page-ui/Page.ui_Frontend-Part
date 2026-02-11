5c31c2dc9845f2084cb9573333c61076a7fa4528
=Polymath000 <abdalrahmankhaled642004@gmail.com>
2026-02-11 16:46:06 +0200

    feat(logic) : enable the forget passwrod view form validator

--- lib/features/auth/presentation/widgets/forget_pasword_view_form.dart
+++ lib/features/auth/presentation/widgets/forget_pasword_view_form.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pageui/config/routes/on_generate_routes.dart';
+import 'package:pageui/config/themes/app_colors.dart';
 import 'package:pageui/core/custom_widget/custom_button.dart';
+import 'package:pageui/core/helpers/custom_show_snack_bar.dart';
 import 'package:pageui/features/auth/presentation/widgets/auth_text_form_field.dart';
 import 'package:pageui/features/auth/presentation/widgets/custom_row_auth.dart';
+import 'package:pageui/features/auth/presentation/widgets/email_validator.dart';
 import 'package:pageui/features/auth/presentation/widgets/have_an_account_widget.dart';
 import 'package:pageui/features/auth/presentation/widgets/password_text_form_field.dart';
 import 'package:pageui/features/auth/presentation/widgets/verify_o_t_p_widget.dart';
@@ -14,6 +18,34 @@ class ForgetPaswordViewForm extends StatefulWidget {
 }
 
 class _ForgetPaswordViewFormState extends State<ForgetPaswordViewForm> {
+  GlobalKey<FormState> formKeyEmailCheck = GlobalKey<FormState>();
+  GlobalKey<FormState> formKeyCodeVerify = GlobalKey<FormState>();
+  GlobalKey<FormState> formKeyPasswordReset = GlobalKey<FormState>();
+
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+  late final TextEditingController _confirmPasswordController;
+  late List<TextEditingController> controllers;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  @override
+  void initState() {
+    super.initState();
+    controllers = List.generate(5, (_) => TextEditingController());
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _confirmPasswordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    controllers.forEach((controller) => controller.dispose());
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -28,7 +60,7 @@ class _ForgetPaswordViewFormState extends State<ForgetPaswordViewForm> {
           ),
 
         SizedBox(
-          height: 230,
+          height: 270,
           child: PageView(
             controller: _controller,
             physics: const NeverScrollableScrollPhysics(),
@@ -40,46 +72,140 @@ class _ForgetPaswordViewFormState extends State<ForgetPaswordViewForm> {
   }
 
   Widget _emailStep() {
-    String email;
     return Form(
+      autovalidateMode: autovalidateMode,
+      key: formKeyEmailCheck,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           customRowAuth(hint: "Type Your Email"),
           const SizedBox(height: 4),
           AuthTextFormField(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'This field is required';
-              }
-              return null;
-            },
+            controller: _emailController,
+            validator: EmailValidator,
           ),
           const SizedBox(height: 4),
           HaveAnAccountWidget(),
           const SizedBox(height: 20),
-          CustomButton(title: 'Send Code', onPressed: nextStep),
+          CustomButton(
+            title: 'Send Code',
+            onPressed: () {
+              if (formKeyEmailCheck.currentState!.validate()) {
+                FocusScope.of(context).unfocus();
+                showWebSnackBar(context: context, message: 'Check Your Email.');
+                formKeyEmailCheck.currentState!.reset();
+                nextStep();
+              } else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
         ],
       ),
     );
   }
 
   Widget _codeStep() {
-    // TODO: Implement code verification logic
-    return Column(children: [VerifyOTPWidget(onPressed: nextStep)]);
+    autovalidateMode = AutovalidateMode.disabled;
+    return Form(
+      key: formKeyCodeVerify,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const Text(
+            "VERIFY OTP",
+            style: TextStyle(
+              color: AppColors.primaryColor,
+              fontSize: 22,
+              letterSpacing: 1.5,
+              overflow: TextOverflow.clip,
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            "ENTER THE 5-DIGIT CODE SENT TO YOU",
+            style: TextStyle(color: AppColors.primaryColor, fontSize: 13),
+          ),
+          const SizedBox(height: 24),
+
+          VerifyOTPWidget(controllers: controllers),
+          const SizedBox(height: 30),
+          CustomButton(
+            title: "VERIFY",
+            onPressed: () {
+              for (var controller in controllers) {
+                if (controller.text.isEmpty) {
+                  showWebSnackBar(
+                    context: context,
+                    message: 'OTP not completed.',
+                    backgroundColor: AppColors.red,
+                    textColor: AppColors.white,
+                  );
+                  return;
+                }
+              }
+              if (formKeyCodeVerify.currentState!.validate()) {
+                FocusScope.of(context).unfocus();
+                showWebSnackBar(context: context, message: 'OTP Verified.');
+                formKeyCodeVerify.currentState!.reset();
+                nextStep();
+              } else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _resetPasswordStep() {
-    String Password;
     return Form(
+      key: formKeyPasswordReset,
+      autovalidateMode: autovalidateMode,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           customRowAuth(hint: "New Password"),
           const SizedBox(height: 4),
-          PasswordTextFormField(),
+          PasswordTextFormField(controller: _passwordController),
+          const SizedBox(height: 16),
+          customRowAuth(hint: "Confirm Password"),
+          const SizedBox(height: 4),
+          PasswordTextFormField(controller: _confirmPasswordController),
           const SizedBox(height: 20),
-          CustomButton(title: 'Reset Password', onPressed: () {}),
+          CustomButton(
+            title: 'Reset Password',
+            onPressed: () {
+              if (formKeyPasswordReset.currentState!.validate()) {
+                if (_confirmPasswordController.text ==
+                    _passwordController.text) {
+                  FocusScope.of(context).unfocus();
+                  showWebSnackBar(
+                    context: context,
+                    message: 'Password Reset Successfully.',
+                  );
+                  formKeyPasswordReset.currentState!.reset();
+                  AppRoutes.pop(context);
+                } else {
+                  showWebSnackBar(
+                    context: context,
+                    message: "Password and Confirm Password must be same",
+                    backgroundColor: AppColors.red,
+                    textColor: AppColors.white,
+                  );
+                }
+              } else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
         ],
       ),
     );
--- lib/features/auth/presentation/widgets/verify_o_t_p_widget.dart
+++ lib/features/auth/presentation/widgets/verify_o_t_p_widget.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:pageui/config/themes/app_colors.dart';
-import 'package:pageui/core/custom_widget/custom_button.dart';
 
 class VerifyOTPWidget extends StatefulWidget {
-  const VerifyOTPWidget({super.key, required this.onPressed});
-  final void Function()? onPressed;
+  VerifyOTPWidget({super.key, required this.controllers});
+  List<TextEditingController> controllers;
   @override
   State<VerifyOTPWidget> createState() => _VerifyOTPWidgetState();
 }
 
 class _VerifyOTPWidgetState extends State<VerifyOTPWidget> {
-  final List<TextEditingController> _controllers = List.generate(
-    5,
-    (_) => TextEditingController(),
-  );
-
   final List<FocusNode> _focusNodes = List.generate(5, (_) => FocusNode());
 
   void _onChanged(String value, int index) {
@@ -30,38 +24,16 @@ class _VerifyOTPWidgetState extends State<VerifyOTPWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text(
-          "VERIFY OTP",
-          style: TextStyle(
-            color: AppColors.primaryColor,
-            fontSize: 22,
-            letterSpacing: 1.5,
-            overflow: TextOverflow.clip,
-          ),
-        ),
-        const SizedBox(height: 8),
-        const Text(
-          "ENTER THE 5-DIGIT CODE SENT TO YOU",
-          style: TextStyle(color: AppColors.primaryColor, fontSize: 13),
-        ),
-        const SizedBox(height: 24),
-
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(5, (index) {
             return Flexible(
-              // Makes the box responsive to available width
               child: Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 4,
-                ), // Reduced margin for tight screens
-                constraints: const BoxConstraints(
-                  maxWidth: 50,
-                  minWidth: 40,
-                ), // Keeps boxes from getting too huge or tiny
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                constraints: const BoxConstraints(maxWidth: 50, minWidth: 40),
                 height: 60,
                 decoration: BoxDecoration(
-                  color: Colors.black,
+                  color: AppColors.black,
                   border: Border.all(color: AppColors.primaryColor, width: 1.5),
                   boxShadow: [
                     BoxShadow(
@@ -71,7 +43,7 @@ class _VerifyOTPWidgetState extends State<VerifyOTPWidget> {
                   ],
                 ),
                 child: TextField(
-                  controller: _controllers[index],
+                  controller: widget.controllers[index],
                   focusNode: _focusNodes[index],
                   textAlign: TextAlign.center,
                   maxLength: 1,
@@ -92,9 +64,6 @@ class _VerifyOTPWidgetState extends State<VerifyOTPWidget> {
             );
           }),
         ),
-
-        const SizedBox(height: 30),
-        CustomButton(title: "VERIFY", onPressed: widget.onPressed),
       ],
     );
   }
